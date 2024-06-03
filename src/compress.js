4fe4f23c8aff23cd22985e0afc2735be5219c454
DemonGodXY <gfxks457@proton.me>
2024-06-03 11:42:10 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -12,7 +12,7 @@ function compress(req, res, input) {
     .grayscale(req.params.grayscale)
     .toFormat(format, {
       quality: req.params.quality,
-      effort: 0,
+      effort: 1,
       chromaSubsampling: '4:2:0'
       
     })
